7f45d05bfa4adb3ee498ad054491e30ef8fc0920
Benoit Laurent <benoit.jacques.felix@gmail.com>
2019-10-14 22:58:48 +0200

    blank sheet nothing here

--- container/components/page.js
+++ container/components/page.js
@@ -1,5 +1,3 @@
-import { routes } from '../config'
-
 export default class ContainerPage extends HTMLElement {
   constructor() {
     super()
@@ -9,80 +7,5 @@ export default class ContainerPage extends HTMLElement {
     return 'container-page'
   }
 
-  connectedCallback() {
-    const appModule = routes.find(route =>
-      route.exact
-        ? route.path === location.pathname
-        : location.pathname.startsWith(route.path)
-    ).module
-
-    const frameworkRoot = this.createElementFromID(appModule)
-    this.appendChild(frameworkRoot)
-
-    this.loadScripts(
-      appModule.scripts.map(scriptPath => appModule.url + scriptPath)
-    )
-    this.loadStyles(
-      appModule.styles.map(stylePath => appModule.url + stylePath)
-    )
-  }
-
-  loadScripts(scripts) {
-    return this.promiseSerial(
-      scripts.map(scriptPath => () =>
-        new Promise((resolve, reject) => {
-          const previousScript = document.getElementById(scriptPath)
-          if (previousScript) {
-            previousScript.remove()
-          }
-          const script = document.createElement('script')
-          script.src = scriptPath
-          script.id = scriptPath
-          script.defer = true
-          script.type = 'text/javascript'
-          script.onload = resolve
-          script.onerror = reject
-          document.body.appendChild(script)
-        })
-      )
-    )
-  }
-
-  loadStyles(styles) {
-    return this.promiseSerial(
-      styles.map(stylePath => () =>
-        new Promise((resolve, reject) => {
-          const previousStyle = document.getElementById(stylePath)
-          if (previousStyle) {
-            previousStyle.remove()
-          }
-          const style = document.createElement('link')
-          style.href = stylePath
-          style.id = stylePath
-          style.rel = 'stylesheet'
-          style.onload = resolve
-          style.onerror = reject
-          document.head.appendChild(style)
-        })
-      )
-    )
-  }
-
-  promiseSerial(funcs, init) {
-    return funcs.reduce(
-      (promise, func) => promise.then(func),
-      Promise.resolve(init)
-    )
-  }
-
-  createElementFromID(appModule) {
-    let root = null
-    if (appModule.tag) {
-      root = document.createElement(appModule.tag)
-    } else {
-      root = document.createElement('div')
-      root.id = appModule.id
-    }
-    return root
-  }
+  connectedCallback() {}
 }
