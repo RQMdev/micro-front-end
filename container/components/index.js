0f5b8523589eabc4c7ab218886a9761807e0ae68
Benoit Laurent <benoit.jacques.felix@gmail.com>
2019-10-14 23:28:38 +0200

    all application on board

--- container/components/index.js
+++ container/components/index.js
@@ -1,5 +1,2 @@
 export { default as ContainerApp } from './app'
-export { default as ContainerLink } from './link'
-export { default as ContainerNavbar } from './navbar'
 export { default as ContainerPage } from './page'
-export { default as ContainerRouter } from './router'
--- container/config.js
+++ container/config.js
@@ -1,56 +1,53 @@
+const reactModule = {
+  id: 'react-app',
+  url: 'http://localhost:3000',
+  scripts: [
+    '/static/js/runtime-main.js',
+    '/static/js/main.chunk.js',
+    '/static/js/2.chunk.js'
+  ],
+  styles: ['/static/css/main.chunk.css']
+}
+
+const angularModule = {
+  tag: 'app-root',
+  url: 'http://localhost:4200',
+  scripts: [
+    '/main-es2015.js',
+    '/polyfills-es2015.js',
+    '/runtime-es2015.js',
+    '/styles-es2015.js',
+    '/vendor-es2015.js'
+  ],
+  styles: []
+}
+
+const vueModule = {
+  id: 'vue-app',
+  url: 'http://localhost:8080',
+  scripts: ['/js/app.js', '/js/chunk-vendors.js'],
+  styles: ['/css/app.css']
+}
+
 export const routes = [
   {
     path: '/',
     exact: true,
-    module: {
-      id: 'react-app',
-      url: 'http://localhost:3000',
-      scripts: [
-        '/static/js/runtime-main.js',
-        '/static/js/main.chunk.js',
-        '/static/js/2.chunk.js'
-      ],
-      styles: ['/static/css/main.chunk.css']
-    }
+    module: reactModule
   },
   {
     path: '/chien',
     title: 'Chien',
-    module: {
-      tag: 'app-root',
-      url: 'http://localhost:4200',
-      scripts: [
-        '/main-es2015.js',
-        '/polyfills-es2015.js',
-        '/runtime-es2015.js',
-        '/styles-es2015.js',
-        '/vendor-es2015.js'
-      ],
-      styles: []
-    }
+    module: angularModule
   },
   {
     path: '/chat',
     title: 'Chat',
-    module: {
-      id: 'react-app',
-      url: 'http://localhost:3000',
-      scripts: [
-        '/static/js/runtime-main.js',
-        '/static/js/main.chunk.js',
-        '/static/js/2.chunk.js'
-      ],
-      styles: ['/static/css/main.chunk.css']
-    }
+    module: reactModule
   },
   {
     path: '/canard',
     title: 'Canard',
-    module: {
-      id: 'vue-app',
-      url: 'http://localhost:8080',
-      scripts: ['/js/app.js', '/js/chunk-vendors.js'],
-      styles: ['/css/app.css']
-    }
+    module: vueModule
   }
 ]
